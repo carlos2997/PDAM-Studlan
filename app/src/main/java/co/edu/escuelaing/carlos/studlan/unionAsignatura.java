b8af3098843d2c69dc9bb11c768eaff3ffcc34ea
Carlos Alberto Ramirez Otero <carlosramirez-2997@hotmail.com>
2017-12-04 21:49:30 -0500

    Arreglo temporal Studlan

---
+++ app/src/main/java/co/edu/escuelaing/carlos/studlan/unionAsignatura.java
@@ -0,0 +1,20 @@
+package co.edu.escuelaing.carlos.studlan;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class unionAsignatura extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_union_asignatura);
+
+
+    }
+
+}
